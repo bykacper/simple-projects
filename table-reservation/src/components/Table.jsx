84b7b12d1ac01caa73f45bb2ff38a03e11a85427
Kacper <webbykacper@gmail.com>
2024-12-12 18:48:57 +0100

    improvments in table reservations

--- table-reservation/src/components/Table.jsx
+++ table-reservation/src/components/Table.jsx
@@ -1,59 +1,126 @@
 import React, { useEffect, useState } from "react";
 import closePic from '../images/close.png';
 
-export default function Table( { table } ) {
+export default function Table({ table }) {
     const [tableId, setTableId] = useState(null);
     const [tableInfoVisibility, setTableInfoVisibility] = useState(false);
     const [bookVisibility, setBookVisibility] = useState(true);
     const [reservationVisibility, setReservationVisibility] = useState(false);
-    
-    const showTableInfo = () => {
-        setTableInfoVisibility(true);
-    }
+    const [reservedHours, setReservedHours] = useState([]);
+    const [unavailableHours, setUnavailableHours] = useState([]);
 
-    const hideTableInfo = event => {
+    useEffect(() => {
+        setTableId(table); 
+    }, []);
+
+    const hideTableInfo = (event) => {
         event.stopPropagation();
         setTableInfoVisibility(false);
-    }
+    };
 
     const showReservations = () => {
         setReservationVisibility(true);
         setBookVisibility(false);
-    }
-    
+    };
+
     const showBooks = () => {
         setReservationVisibility(false);
         setBookVisibility(true);
-    }
- 
+    };
 
-    useEffect(() => {
-        setTableId(table);
-    }, [tableId])
+    const addReservation = (newHour) => {
+        const isUnavailable = unavailableHours.includes(newHour);
+
+        if (isUnavailable) {
+            alert(`${newHour} jest niedostępna.`);
+        } else {
+            setReservedHours(prevHours => {
+                if (prevHours.includes(newHour))
+                    return prevHours;
+                return [...prevHours, newHour];
+            });
+        }
+    }
 
     return (
-        <div className="table" onClick={showTableInfo}>
-            { tableInfoVisibility && (
+        <div className="table" onClick={() => setTableInfoVisibility(true)}>
+            {tableInfoVisibility && (
                 <section className="table-info">
                     <div>
-                        <img src={closePic} alt="close" onClick={hideTableInfo}/>
-                        <button onClick={showBooks}> zarezerwuj </button>
-                        <button onClick={showReservations}> rezerwacje </button>
+                        <img src={closePic} alt="close" onClick={hideTableInfo} />
+                        <button onClick={showBooks}>Zarezerwuj</button>
+                        <button onClick={showReservations}>Rezerwacje</button>
                     </div>
                     <div>
-                        {bookVisibility && (
-                        <div className="book">
-                            tu mozna zarezerwowac {tableId}
-                        </div>
-                        )}
+                    {bookVisibility && (
+                        <BookingSection 
+                            tableId={tableId} 
+                            setReservedHours={(newHour) => addReservation(newHour)} />
+                    )}
                         {reservationVisibility && (
-                        <div>
-                            tu mozna zobaczyc rezerwacje dla {tableId}
-                        </div>
+                            <Reservations tableId={tableId}
+                                        reservedHours={reservedHours}/>
                         )}
                     </div>
                 </section>
             )}
+        </div>
+    );
+}
+
+function BookingSection({ tableId, setReservedHours }) {
+    const [booker, setBooker] = useState("");
+    const [hour, setHour] = useState("");
+
+    const reserveTable = () => {
+        if (!booker || !hour) {
+            alert("Proszę wypełnić wszystkie pola.");
+            return;
+        }
+
+        setReservedHours(hour);
+    };
+
+    return (
+        <div className="book">
+            <div>
+                <label htmlFor="bookerName">Imię rezerwującego:</label>
+                <input
+                    type="text"
+                    name="bookerName"
+                    onChange={(e) => setBooker(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="bookingHour">Rezerwowana godzina:</label>
+                <input
+                    type="time"
+                    name="bookingHour"
+                    onChange={(e) => setHour(e.target.value)}
+                />
+            </div>
+            <button className="bookButton" onClick={reserveTable}>
+                Rezerwuj
+            </button>
+        </div>
+    );
+}
+
+function Reservations({ reservedHours }) {
+
+    return (
+        <div>
+            <div>
+            {reservedHours.length > 0 ? (
+                reservedHours.map(hour => (
+                    <div>
+                        gość zarezerwował(a) na godzinę {hour}.
+                    </div>
+                ))
+            ) : (
+                <div>Brak rezerwacji.</div>
+            )}
+            </div>
         </div>
     )
 }
